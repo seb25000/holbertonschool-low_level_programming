45bb2b22d600189a433e108a7e75d529124588c3
seb25000 <seb25000@gmail.com>
2024-10-21 16:11:20 +0200

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+		//string initialisation
+ char Mystr[] = "Programming is like building a multilingual puzzle";
+
+	 int val = puts(Mystr);
+	                    
+	 return 0;
+}
