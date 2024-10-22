64ef6978a0fea242ea7945bf84e1c2b0912aec86
seb25000 <seb25000@gmail.com>
2024-10-22 10:11:15 +0200

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include  <stdio.h>
 /** 
- * main -main block 
+ * main - main block 
  * Return: 0 
  */ 
 int main(void)
 { 
-puts("\"La programmation, c'est comme construire un puzzle multilingue"); 
+puts("\"Programming is like building a multilingual puzzle"); 
 return (0); 
 }
-
