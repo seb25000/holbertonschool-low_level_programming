886653ce16c1c39ce703f68c38f502e895992a99
seb25000 <seb25000@gmail.com>
2024-10-23 16:16:04 +0200

    2-print_alphabet.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,10 +10,17 @@ int main(void)
 int n;	
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 0)
-printf("%d is positive", n);
-else if (n == 0)
-printf("%d is zero", n);
+if ((n % 10) > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+}
+else if ((n % 10) < 6 && (n % 10) != 0)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+}
 else
-printf("%d is negative", n);
+{
+printf("Last digit of %d is %d and is 0\n", n, n % 10);
+}
 return (0);
+}
