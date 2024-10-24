47a6fffbeec33fd0f8718adc465ddbcc5c66b3bb
seb25000 <seb25000@gmail.com>
2024-10-24 16:01:44 +0200

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,8 +8,12 @@ int main (void)
 {
 char alphabet;
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
 if (alphabet != 'q' && alphabet != 'e')
+{
 putchar(alphabet);
 putchar('\n');
+}
+}
 return (0);
 }
