2e5753b284ac5a5d17b733064ce5043ae244613f
seb25000 <seb25000@gmail.com>
2024-10-24 15:06:18 +0200

    Update file 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -11,6 +11,6 @@ for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 putchar(alphabet);
 for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 putchar(alphabet);
-putchar("\n");
+putchar('\n');
 return (0);
 }
