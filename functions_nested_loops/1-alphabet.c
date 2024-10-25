6d8af2ef0f56a42d37961d58d55f2e2666cdec76
seb25000 <seb25000@gmail.com>
2024-10-25 16:58:50 +0200

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,9 +5,8 @@
  */
 void print_alphabet(void)
 {
-char alphabet = 'a';
-for (; alphabet <= 'z'; alphabet++)
-_putchar(alphabet);
-}
+char l;
+for (l = 'a'; l <= 'z'; l++)
+_putchar(l);
 _putchar('\n');
 }
