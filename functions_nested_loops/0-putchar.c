959391edeb56ae66a23a6ce23481df5013de655c
seb25000 <seb25000@gmail.com>
2024-10-25 13:36:42 +0200

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,18 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char _putchar = '_';
-for (_putchar = '_'; _putchar <= 'r'; _putchar++)
-putchar (_putchar);
-return (0);
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
+return(0);
 }
