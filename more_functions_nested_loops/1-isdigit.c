170a9462784bd16bf2ba6073b954682dce285137
seb25000 <seb25000@gmail.com>
2024-10-28 13:59:34 +0100

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - check the digit
+ * @c: the digit
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+return (0);
+}
