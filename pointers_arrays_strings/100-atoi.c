1e894467370147d92d9c7f98e55bd71f284ff43c
seb25000 <seb25000@gmail.com>
2024-11-04 21:10:53 +0100

    100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _atoi - check the code
+ *
+ * @s: pointer
+ * Return: Always 0.
+ */
+int _atoi(char *s)
+{
+int sign = 1;
+int result = 0;
+int i = 0;
+if (s[0] == '-')
+{
+sign = -1;
+i++;
+}
+if (s[0] == '+')
+{
+i++;
+}
+while (s[i] != '\0')
+{
+if (s[i] >= '0' && s[i] <= '9')
+{
+result = result * 10 + (s[0] - '0');
+}
+else
+{
+break;
+}
+i++;
+}
+result = result *sign;
+return (result);
+}
