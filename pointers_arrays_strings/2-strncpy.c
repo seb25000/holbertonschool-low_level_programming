af1ed3e6193805d9262a0f37c21a3b35e9cc0250
seb25000 <seb25000@gmail.com>
2024-11-05 13:48:06 +0100

    Update file

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,15 +12,12 @@ char *_strncpy(char *dest, char *src, int n)
 int i, j;
 i = 0;
 j = 0;
-while (dest[i] != '\0')
+while (src[i] != '\0')
 {
+while (i != '\0')
+dest[i] = src[i];
+while (i < n)
 i++;
-}
-while (src[j] != '\0' && j < n)
-{
-dest[i] = src[j];
-i++;
-j++;
 }
 dest[i] = '\0';
 return (dest);
