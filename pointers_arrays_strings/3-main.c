3bfd499412b0eb9f5ec8b875b8222683b844ea95
seb25000 <seb25000@gmail.com>
2024-11-05 14:24:33 +0100

    3-strcmp.c

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - check the code
  *
@@ -6,8 +7,10 @@
  */
 int main(void)
 {
-char *str;
-str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-_puts(str);
+char s1[] = "Hello";
+char s2[] = "World!";
+printf("%d\n", _strcmp(s1, s2));
+printf("%d\n", _strcmp(s2, s1));
+printf("%d\n", _strcmp(s1, s1));
 return (0);
 }
