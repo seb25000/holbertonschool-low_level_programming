4542b848723ae511d9e39b10adf4c219d09ec5e6
seb25000 <seb25000@gmail.com>
2024-11-06 14:13:03 +0100

    3-strspn.c

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-char s1[] = "Hello";
-char s2[] = "World!";
-printf("%d\n", _strcmp(s1, s2));
-printf("%d\n", _strcmp(s2, s1));
-printf("%d\n", _strcmp(s1, s1));
+char *s = "hello, world";
+char *f = "oleh";
+unsigned int n;
+n = _strspn(s, f);
+printf("%u\n", n);
 return (0);
 }
