33f5503712489ebded98b2d51a83a337f3267ed0
seb25000 <seb25000@gmail.com>
2024-11-06 14:02:22 +0100

    2-strchr.c

--- pointers_arrays_strings/2-main.c
+++ pointers_arrays_strings/2-main.c
@@ -1,38 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strncpy - check the code
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-char s1[98];
-char *ptr;
-int i;
-for (i = 0; i < 98 - 1; i++)
-{s1[i] = '*';
-}
-s1[i] = '\0';
-printf("%s\n", s1);
-ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-printf("%s\n", s1);
-printf("%s\n", ptr);
-ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
-printf("%s", s1);
-printf("%s", ptr);
-for (i = 0; i < 98; i++)
+char *s = "hello";
+char *f;
+f = _strchr(s, 'l');
+if (f != NULL)
 {
-if (i % 10)
-{
-printf(" ");
+printf("%s\n", f);
 }
-if (!(i % 10) && i)
-{
-printf("\n");
-}
-printf("0x%02x", s1[i]);
-}
-printf("\n");
 return (0);
 }
