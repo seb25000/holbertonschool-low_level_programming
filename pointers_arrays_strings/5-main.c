bd53aa3ddd5aac06133e7fb8740bf912f8bd8194
seb25000 <seb25000@gmail.com>
2024-11-06 15:28:07 +0100

    5-strstr.c

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-char str[] = "Look up!\n";
-char *ptr;
-ptr = string_toupper(str);
-printf("%s", ptr);
-printf("%s", str);
+char *s = "hello, world";
+char *f = "world";
+char *t;
+t = _strstr(s, f);
+printf("%s\n", t);
 return (0);
 }
