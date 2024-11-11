3deaadd894f970332237401e8984c2ed9f272d51
seb25000 <seb25000@gmail.com>
2024-11-11 20:00:21 +0100

    2-str_concat.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * create_array - Creates an array of chars and initializes it with a specific char.
+ * create_array - Creates an array of chars and initializes it with char.
  * @size: The size of the array to be initialized.
  * @c: The specific char to intialize the array with.
  *
- * Return: If size == 0 or the function fails - NULL. Otherwise - a pointer to the array.
+ * Return: If size == 0 or the function fails - NULL. Otherwise.
  */
 char *create_array(unsigned int size, char c)
 {
