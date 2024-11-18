6a85b4c494185f39c33da6dd3c255fa30a819885
seb25000 <seb25000@gmail.com>
2024-11-18 14:23:51 +0100

    0-print_name.c

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,8 +1,8 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-calc num1 operator num2;
 int _putchar(char c);
 #endif
