7886d039dd7c1b64fa395f717101629b3bbee9c2
seb25000 <seb25000@gmail.com>
2024-11-22 19:25:42 +0100

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,6 +1,6 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 /**
  * print_all - print anything passed if char, int, float, or string
  * _printchar - print char type element from va_list
@@ -11,39 +11,40 @@ void print_all(const char * const format, ...)
 {
 va_list args;
 int i = 0;
-char *str;
 char c;
-int n;
+int num;
 float f;
+char *s;
 va_start(args, format);
 while (format && format[i])
 {
-if (format[i] == 'c')
+if (i > 0)
+printf(", ");
+switch (format[i])
 {
+case 'c':
 c = va_arg(args, int);
 printf("%c", c);
-}
-else if (format[i] == 'i')
-{
-n = va_arg(args, int);
-printf("%d", n);
-}
-else if (format[i] == 'f')
-{
+break;
+case 'i':
+num = va_arg(args, int);
+printf("%d", num);
+break;
+case 'f':
 f = va_arg(args, double);
 printf("%f", f);
-}
-else if (format[i] == 's')
-{
-str = va_arg(args, char *);
-if (str)
-printf("%s", str);
+break;
+case 's':
+s = va_arg(args, char *);
+if (s)
+printf("%s", s);
 else
 printf("(nil)");
+break;
+default:
+break;
 }
 i++;
-if (format[i] != '\0')
-printf(", ");
 }
 va_end(args);
 printf("\n");
