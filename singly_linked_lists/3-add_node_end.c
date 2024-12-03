d2bb7fba3ae6378388567ffd6b979f1ab6a92600
seb25000 <seb25000@gmail.com>
2024-12-03 14:19:18 +0100

    3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -12,11 +12,14 @@ list_t *add_node_end(list_t **head, const char *str)
 list_t *current;
 list_t *new_node;
 int c;
+char *dup_str;
+dup_str = strdup(str);
+if (!dup_str)
+return (NULL);
 current = *head;
 while (current && current->next != NULL)
 current = current->next;
 for (c = 0; dup_str[c] != '\0'; c++)
-;
 new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
 {
