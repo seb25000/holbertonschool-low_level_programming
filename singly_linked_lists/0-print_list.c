a4d1676dba9d1f9c1d13be2ab2f3b7cf60466707
seb25000 <seb25000@gmail.com>
2024-12-05 16:56:46 +0100

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -7,15 +7,19 @@
  */
 size_t print_list(const list_t *h)
 {
-const list_t *current;
-size_t c;
-c = 0;
-current = h;
-while (current != NULL)
+size_t node = 0;
+while (h != NULL)
 {
-printf("[%d] %s\n", current->len, current->str);
-current = current->next;
-c++;
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
 }
-return (c);
+else
+{
+printf("[%u] %s\n", h->len, h->str);
+}
+node++;
+h = h->next;
+}
+return (node);
 }
